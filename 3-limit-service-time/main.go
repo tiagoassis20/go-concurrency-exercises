75ad6e742c502bfa4b49939ec2f9c8449fbf21ff
Tiago Assis <tiago.assis@estrategiaconcursos.com.br>
2022-03-28 14:45:17 -0300

    adiciona  controle de resposta e tempo de execução no limit service time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,19 +10,60 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mux       sync.Mutex
 }
+
+const maxFreeTime = 10
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	// if !u.IsPremium && u.TimeUsed > 10 {
+	// 	return false
+	// }
+	// start := time.Now()
+	// process()
+	// elapsed := time.Now().Sub(start).Seconds()
+	// u.TimeUsed += int64(elapsed)
+	// return u.IsPremium || u.TimeUsed <= 10
+
+	done := make(chan bool)
+	go func(chan<- bool) {
+		process()
+		done <- true
+	}(done)
+	for {
+		select {
+		case <-done:
+			return true
+		case <-time.Tick(time.Second):
+			{
+				u.mux.Lock()
+
+				u.TimeUsed++
+
+				if !u.IsPremium && u.TimeUsed > maxFreeTime {
+					u.mux.Unlock()
+					return false
+				}
+				u.mux.Unlock()
+
+			}
+
+		}
+	}
+
 }
 
 func main() {
